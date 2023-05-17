139e5de2c3d1b77155c61e84cb16c5ca19add892
TommyPREEL <45074150+TommyPREEL@users.noreply.github.com>
2023-05-17 22:02:31 +0200

    split challenges by tabs

--- back/models/challenges.js
+++ back/models/challenges.js
@@ -19,9 +19,17 @@ function getAllChallenges(){
 }
 
 function startChallenge(challenge){
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hour = String(currentDate.getHours()).padStart(2, '0');
+    const minute = String(currentDate.getMinutes()).padStart(2, '0');
+    const second = String(currentDate.getSeconds()).padStart(2, '0');
+    const formattedDateTime = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
     return new Promise((resolve, reject) => {
-        const sql = `INSERT INTO Challenges (id_users_challenger, id_users_challenged, id_mini_games, status) VALUES (?, ?, ?, ?);`;
-        db.run(sql, [challenge.challenger.id_users, challenge.challenged.id_users, challenge.challenge.id_mini_games, status.WAITING], (err, rows) => {
+        const sql = `INSERT INTO Challenges (id_users_challenger, id_users_challenged, id_mini_games, status, challenges_date) VALUES (?, ?, ?, ?, ?);`;
+        db.run(sql, [challenge.challenger.id_users, challenge.challenged.id_users, challenge.challenge.id_mini_games, status.WAITING, formattedDateTime], (err, rows) => {
             if (err) {
                 throw err;
             }
@@ -33,7 +41,7 @@ function startChallenge(challenge){
 function challengesListToAccept(id_user){
     return new Promise((resolve, reject) => {
         const sql = `SELECT * from challenges JOIN Users ON id_users = id_users_challenger JOIN Users ON id_users = id_users_challenged WHERE id_users_challenged = ? AND status = ?`;
-        db.all(sql, [challenge.challenged.id_users, status.WAITING], (err, rows) => {
+        db.all(sql, [id_users, status.WAITING], (err, rows) => {
             if (err) {
                 throw err;
             }
